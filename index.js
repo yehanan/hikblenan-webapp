494bc308d807fb096e8cf7319b23b16bb5ae6d30
Yehanan Tezera <118446757+yehanan@users.noreply.github.com>
2023-11-03 00:43:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const sendEmail = require('./sendEmail');
-
+const Mailjet = require('mailjet');
 
 const app = express();
+
+// Initialize Mailjet with API and Secret keys
+const mailjet = new Mailjet({
+  apiKey: 'b1575398469ebf1fc9e0cbccb26894a8',
+  apiSecret: '7e98350cf57a2fb61df470f1bfda890d'
+});
 
 // Define a route to handle requests to send an email
 app.post('/sendEmail', async (req, res) => {
@@ -14,7 +20,8 @@ app.post('/sendEmail', async (req, res) => {
 
   // Send an email to the parent
   try {
-    await sendEmail(parentEmail, studentName, totalScore, gradeLetter);
+    // Implement your email sending logic here using the 'mailjet' instance
+    await sendEmail.sendEmail(mailjet, parentEmail, studentName, totalScore, gradeLetter);
     res.status(200).send('Email sent successfully');
   } catch (err) {
     res.status(500).send('Failed to send email');
