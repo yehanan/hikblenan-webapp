bb7bbeb5bd15818d5c02ee383b47c73f4701aaf9
Yehanan Tezera <118446757+yehanan@users.noreply.github.com>
2023-11-02 23:34:50 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const sendEmail = require('./sendEmail');
+
+const app = express();
+
+// Define a route to handle requests to send an email
+app.post('/sendEmail', async (req, res) => {
+  // Get the parent email address and student information from the request
+  const parentEmail = req.body.parentEmail;
+  const studentName = req.body.studentName;
+  const totalScore = req.body.totalScore;
+  const gradeLetter = req.body.gradeLetter;
+
+  // Send an email to the parent
+  try {
+    await sendEmail(parentEmail, studentName, totalScore, gradeLetter);
+    res.status(200).send('Email sent successfully');
+  } catch (err) {
+    res.status(500).send('Failed to send email');
+  }
+});
+
+// Start the Express server
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
